727debcc81460dd17353a1de0abae46bc1820c56
zy-98 <59222884+zy-98@users.noreply.github.com>
2020-02-04 16:54:04 +0800

    练习

---
+++ 循环删除/循环删除/循环删除.cpp
@@ -0,0 +1,59 @@
+﻿// 循环删除.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int value;
+	bool isDeleted;
+};
+int main()
+{
+	Node a[1001];
+	int i, n = 1000;
+	for (i = 0; i < n; ++i)
+	{
+		a[i].value = i;
+		a[i].isDeleted = false;
+
+	}
+	int cur = 0, count, num = n;
+	while (num != 1)
+	{
+		count = 0;
+		while (count != 2)
+		{
+			cur = (cur + 1) % n;
+			if (a[cur].isDeleted == false)
+			{
+				count++;
+			}
+		}
+		a[cur].isDeleted = true;
+		// cout<<a[cur].value<<"  ";
+		while (a[cur].isDeleted != false)
+		{
+			cur = (cur + 1) % n;
+		}
+		--num;
+	}
+	cout << endl;
+	cout << cur << endl;
+	system("pause");
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
